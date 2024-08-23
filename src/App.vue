befb614016637fd83c2da9c5398433d0f011ca49
Jeison SR <jeison.sr.cursos@gmail.com>
2024-08-23 18:32:37 -0300

    Configurção do projeto.

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,57 @@
 <script>
 // Código Javascript aqui
+  export default {
+    data() {
+        return {
+            message: "Bem Vindo Fulano!!!",
+            corpo: "Corpo do texto",
+            count: 0
+        }
+    }
+  }
 </script>
 
 <template>
-  <!-- Código HTML aqui -->
-  <header>
-    <!-- Cabeçalho -->
-    <h1>Cabeçalho</h1>
+  <header class="mb-5" id="header">
+      <!-- Cabeçalho -->
+      <nav class="navbar navbar-light border-bottom">
+          <div class="container-fluid">
+            <a class="navbar-brand" href="#">
+              <img src="./assets/img/logo-firjan.png" alt="Logo" class="logo-firjan">
+            </a>
+          </div>
+      </nav>
+      <div class="container-fluid mt-5 text-center">
+          <h1>Curso Programador FrontEnd</h1>
+          <!-- <h2>Disciplina: Desenvolvimento Web</h2>
+          <h3>{{ message }}</h3>
+          <h4>{{ corpo }}</h4> -->
+      </div>
   </header>
-  <main>
-    <!-- Conteúdo -->
-    <h2>Conteúdo</h2>
+  <main class="container" id="app">
+      <!-- Conteúdo -->
+      <h3>{{ message }}</h3>
+      <h4>{{ corpo }}</h4>
+      <hr>
+
+      <p v-show="count > 0">Contagem atual: {{ count }}</p>
+
+      <button class="btn btn-warning" @click="count--" :disabled="count <= 0">Decrementar</button> | 
+      <button class="btn btn-primary" @click="count++">Incrementar</button>
+
+      <div v-if="count > 0">
+          <p>Objeto count está sendo atualizado.</p>
+      </div>
+      <div v-else>
+          <p>Objeto count tem como valor o 0.</p>
+      </div>
+
   </main>
-  <footer>
-    <!-- Rodapé -->
-    <h3>Rodapé</h3>
+  <footer class="container-fluid fixed-bottom bg-dark">
+      <!-- Rodapé -->
+        <div class="f-info">
+          <span>Desenvolvido por [SEU NOME] 2024&COPY;</span>
+        </div>
   </footer>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/js/bootstrap.bundle'
+import './assets/css/style.css'
 import App from './App.vue'
 
 createApp(App).mount('#app')
